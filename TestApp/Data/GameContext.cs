688a928e33f782f9d277a985ffe2f1731bc78d5b
kubaabdybekov <abdybekovkuba@gmail.com>
2021-05-22 02:09:50 +0500

    Добавьте файлы проекта.

---
+++ TestApp/Data/GameContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApp.Models;
+
+namespace TestApp.Data
+{
+    public class GameContext: DbContext
+    
+       {
+            public GameContext(DbContextOptions<GameContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Game> Game { get; set; }
+        }
+}
